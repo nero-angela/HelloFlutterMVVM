8192a966385641d03ca1c5d86e4427aaad78ba4c
nero <devstory.co.kr@gmail.com>
2020-06-08 11:29:00 +0900

    Draw login_view

--- lib/screen/login/login_view.dart
+++ lib/screen/login/login_view.dart
@@ -7,8 +7,52 @@ class LoginView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        body: Center(
-          child: Text('Login View'),
+        body: GestureDetector(
+          // hide softkeyboard when click the background
+          onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+          behavior: HitTestBehavior.translucent,
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 32),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'User Id',
+                    border: OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(8),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 16),
+                  width: double.infinity,
+                  height: 50,
+                  child: FlatButton(
+                    color: Colors.pinkAccent,
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      side: BorderSide(color: Colors.red),
+                    ),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
