2fc9541a04bdda74f1f1261ff85063ba9fcf0829
nero <devstory.co.kr@gmail.com>
2020-06-08 11:51:40 +0900

    Add api and user_model

---
+++ lib/service/api.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_tutorial/model/users.dart';
+
+class Api {
+  static const endpoint = 'https://jsonplaceholder.typicode.com';
+
+  Future<User> getUserProfile(int userId) async {
+    try {
+      Response response = await Dio().get('$endpoint/users/$userId');
+      return User.fromJson(json.decode(response.data));
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+}
