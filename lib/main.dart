21cec0eff53466e6f2d6b9b423243a8a0cf1a35f
nero <devstory.co.kr@gmail.com>
2020-06-08 11:06:20 +0900

    Add screen directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/screen/home/home_view.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,19 +17,6 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomeView(),
-    );
-  }
-}
-
-class HomeView extends StatelessWidget {
-  const HomeView({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hello Flutter'),
-      ),
     );
   }
 }
